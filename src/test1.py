edbd669533bca2c312a37aa8b8a3f2acb3697fad
Kaede10 <33061011+Kaede10@users.noreply.github.com>
2024-02-06 16:07:25 +0800

    Create test1.py

---
+++ src/test1.py
@@ -0,0 +1,26 @@
+import json
+import os
+import torch
+from chat_api.configs import configs
+from chat_api.logs import MyLogger
+from transformers import AutoTokenizer, AutoModel, AutoConfig
+
+
+class ChatGLM:
+    def __init__(self) -> None:
+        self.configs = configs
+        self.base_model_path = self.configs.get('chat_glm', 'base_model_path')
+        self.checkpoint_path = self.configs.get('chat_glm', 'checkpoint_path')
+        self.pre_seq_len = self.configs.getint('chat_glm', 'pre_seq_len', fallback=128)
+        self.max_length = self.configs.getint('chat_glm', 'max_length', fallback=2048)
+        self.top_p = self.configs.getfloat('chat_glm', 'top_p')
+        self.temperature = self.configs.getfloat('chat_glm', 'temperature')
+        self.config = AutoConfig.from_pretrained(self.base_model_path, trust_remote_code=True)
+        self.config.pre_seq_len = self.pre_seq_lens
+        self.tokenizer = AutoTokenizer.from_pretrained(self.base_model_path, trust_remote_code=True)
+        self.model = None
+
+    def init_model(self):
+        self.model = AutoModel.from_pretrained(self.base_model_path, config=self.config, trust_remote_code=True)
+        self.model.half().cuda()
+        self.model.eval()
